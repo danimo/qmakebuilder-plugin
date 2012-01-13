361d8ba2c5bf818c4fbde83c1f23cc08c4530173
Daniel Molkentin <daniel.molkentin@nokia.com>
2012-01-13 12:00:50 +0100

    Cleanup and extend:
    - Remove 'make clean' action. It was never really used and is not reliable anyway. scm-based cleaning is preferred
    - Add option to set 'make' binary and options for Windows and Unix on a global bases
    - Substitute environment variables on all fields.
    - Allow to specify extra targets
    - Allow shadow builds

--- src/main/java/hudson/plugins/qmake/QmakeBuilder.java
+++ src/main/java/hudson/plugins/qmake/QmakeBuilder.java
@@ -20,168 +20,220 @@ import javax.servlet.ServletException;
 import java.io.File;
 import java.io.PrintStream;
 import java.io.IOException;
+import java.util.List;
 
 /**
- * Executes <tt>qmake</tt> and <tt>make</tt> as the build process for a QT-based build.
+ * Executes <tt>qmake</tt> and <tt>make</tt> as the build process for a Qt-based build.
  *
+ * @author Daniel Molkentin
  * @author Tyler Mace
  */
 public class QmakeBuilder extends Builder {
 
-    private String projectFile;
-    private String extraArguments;
-    @Deprecated private transient String extraConfig; // unused, deprecated
-    private boolean cleanBuild;
+	private String projectFile;
+	private String extraArguments;
+	private String extraTargets;
+	private String shadowBuildDir;
+	private boolean useShadowBuild;
 
-    private QmakeBuilderImpl builderImpl;
-    
-    @DataBoundConstructor
-    public QmakeBuilder(String projectFile, String extraArguments) {
-      this.projectFile = projectFile;
-      this.extraArguments = extraArguments;
-      this.cleanBuild = false;
-      builderImpl = new QmakeBuilderImpl();		
-    }
+	private QmakeBuilderImpl builderImpl;
 
-    public String getProjectFile() {
-      return this.projectFile;
-    }
-    
-    public String getExtraArguments() {
-      return this.extraArguments;
-    }
-
-    public boolean getCleanBuild() {
-      return this.cleanBuild;
-    }
-
-    @Override
-    public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-      PrintStream logger = listener.getLogger();
-
-      logger.println("MODULE: " + build.getModuleRoot());
-
-      final boolean isWindows = !launcher.isUnix();
-
-      if (builderImpl == null) {
-	builderImpl = new QmakeBuilderImpl();
-      }
-      EnvVars envVars = build.getEnvironment(listener);
-
-      String theProjectFile;
-      try {
-	theProjectFile = builderImpl.preparePath(envVars, this.projectFile,
-	                               isWindows,
-				       logger);
-      } catch (IOException ioe) {
-	logger.println("IO Exception with the project file: " + projectFile);
-	logger.println(ioe.getMessage());
-	return false;
-      }
-
-      builderImpl.setQmakeBin( envVars, getDescriptor().qmakePath(), isWindows );
-
-      String qmakeCall = builderImpl.buildQMakeCall(theProjectFile, extraArguments );
-
-      FilePath workDir = new FilePath(build.getWorkspace(),
-                                      theProjectFile); 
-      workDir = workDir.getParent();
-      logger.println("QMake call : " + qmakeCall);
-
-      try {
-	int result = launcher.launch().cmds(Util.tokenize(qmakeCall)).envs(envVars).stdout(logger).pwd(workDir).join();
-	if (result != 0) return false;
-
-	String makeExe = "make";
-	if (!launcher.isUnix()) makeExe = "nmake";
-
-	result = launcher.launch().cmds(makeExe).envs(envVars).stdout(logger).pwd(workDir).join();
-
-	if (result != 0) {
-	  return false;
-	} else {
-	  return true;
+	@DataBoundConstructor
+	public QmakeBuilder(String projectFile, String extraArguments, String extraTargets, String shadowBuildDir, boolean useShadowBuild) {
+		this.projectFile = projectFile;
+		this.extraArguments = extraArguments;
+		this.extraTargets = extraTargets;
+		this.shadowBuildDir = shadowBuildDir;
+		this.useShadowBuild = useShadowBuild;
 	}
-      } catch (IOException e) {
-	e.printStackTrace();
-      } catch (InterruptedException ie) {
-	ie.printStackTrace();
-      }
-      return false;
+
+	public String getProjectFile() {
+		return this.projectFile;
+	}
+
+	public String getExtraArguments() {
+		return this.extraArguments;
+	}
+
+	public String getExtraTargets() {
+		return this.extraTargets;
+	}
+
+	public String getShadowBuildDir() {
+		return this.shadowBuildDir;
+	}
+
+	public boolean getUseShadowBuild() {
+		return this.useShadowBuild;
     }
 
-    @Override
-    public DescriptorImpl getDescriptor() {
-      return (DescriptorImpl)super.getDescriptor();
-    }
+	@Override
+	public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+		PrintStream logger = listener.getLogger();
 
-    /**
-     * Descriptor for {@link QmakeBuilder}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
-     * See <tt>views/hudson/plugins/hello_world/QmakeBuilder/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
-     */
-    @Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
-      /**
-       * To persist global configuration information,
-       * simply store it in a field and call save().
-       *
-       * <p>
-       * If you don't want fields to be persisted, use <tt>transient</tt>.
-       */
-      private String qmakePath;
+		logger.println("MODULE: " + build.getModuleRoot());
 
-      public DescriptorImpl() {
-	super(QmakeBuilder.class);
-	load();
-      }
+		final boolean isWindows = !launcher.isUnix();
 
-      /**
-       * Performs on-the-fly validation of the form field 'projectFile'.
-       *
-       * @param value
-       */
-      public FormValidation doCheckProjectFile(@QueryParameter final String value) throws IOException, ServletException {
-	if(value.length()==0)
-	  return FormValidation.error("Please set a project file");
-	if(value.length() < 1)
-	  return FormValidation.warning("Isn't the name too short?");
+		if (this.builderImpl == null) {
+			this.builderImpl = new QmakeBuilderImpl();
+		}
+		EnvVars envVars = build.getEnvironment(listener);
 
-	File file = new File(value);
-	if (file.isDirectory())
-	  return FormValidation.error("Project file is a directory");
+		String theProjectFile = EnvVarReplacer.replace(this.projectFile, envVars).trim();
 
-	//TODO add more checks
-	return FormValidation.ok();
-      }
+		try {
+			this.builderImpl.setQmakeBin(envVars, getDescriptor().getQmakePath(), isWindows);
+		} catch (Exception e) {
+			logger.println("Exception while processing qmake path: " + getDescriptor().getQmakePath());
+			logger.println(e.getMessage());
+			return false;
+		}
 
-      /**
-       * This human readable name is used in the configuration screen.
-       */
-      public String getDisplayName() {
-	return "QMake Build";
-      }
+		FilePath projectFilePath = new FilePath(build.getWorkspace(), theProjectFile);
+		FilePath sourceDir = projectFilePath.getParent();
+		FilePath buildDir;
 
-      @Override
-      public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-        return true;
-      }
+		try {
+			String theShadowBuildDir = this.shadowBuildDir;
+			if (theShadowBuildDir.isEmpty())
+				theShadowBuildDir = "";
+			buildDir = new FilePath(sourceDir, EnvVarReplacer.replace(theShadowBuildDir, envVars));
+			if (this.useShadowBuild) {
+				logger.println("Using shadow build: " + buildDir);
+				buildDir.mkdirs();
+			}
+		} catch(IOException ioe) {
+			logger.println("IO Exception with build directory: " + projectFile);
+			logger.println(ioe.getMessage());
+			return false;
+		}
 
-      @Override
-      public boolean configure(StaplerRequest req, JSONObject o) throws FormException {
-	// to persist global configuration information,
-	// set that to properties and call save().
-	qmakePath = o.getString("qmakePath");
-	save();
-	return super.configure(req, o);
-      }
+		String qmakeCall = builderImpl.buildQMakeCall(projectFilePath, EnvVarReplacer.replace(this.extraArguments, envVars));
+		logger.println("QMake call : " + qmakeCall);
 
-      public String qmakePath() {
-	return qmakePath;
-      }
-    }
+		try {
+			int result = launcher.launch().cmds(Util.tokenize(qmakeCall)).envs(envVars).stdout(logger).pwd(buildDir).join();
+			if (result != 0)
+				return false;
+
+			String makeExe = getDescriptor().getMakeCmdUnix();
+			if (!launcher.isUnix())
+          makeExe = getDescriptor().getMakeCmdWindows();
+
+			makeExe = EnvVarReplacer.replace(makeExe, envVars);
+
+			result = launcher.launch().cmds(Util.tokenize(makeExe)).envs(envVars).stdout(logger).pwd(buildDir).join();
+			if (result != 0)
+				return false;
+
+			// TODO: is tokenize the right choice? (mostly using it for chomp here)
+			String[] targets = Util.tokenize(this.extraTargets);
+			for (String target : targets) {
+				String exec = makeExe + " " + target;
+				result = launcher.launch().cmds(Util.tokenize(exec)).envs(envVars).stdout(logger).pwd(buildDir).join();
+				if (result != 0)
+					return false;
+			}
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl)super.getDescriptor();
+	}
+
+	/**
+	 * Descriptor for {@link QmakeBuilder}. Used as a singleton.
+	 * The class is marked as public so that it can be accessed from views.
+	 *
+	 * <p>
+	 * See <tt>resources/hudson/plugins/qmake/QmakeBuilder/*.jelly</tt>
+	 * for the actual HTML fragment for the configuration screen.
+	 */
+	@Extension
+	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+		/**
+		 * To persist global configuration information,
+		 * simply store it in a field and call save().
+		 *
+		 * <p>
+		 * If you don't want fields to be persisted, use <tt>transient</tt>.
+		 */
+		private String qmakePath;
+		private String makeCmdUnix;
+		private String makeCmdWindows;
+
+		public DescriptorImpl() {
+			super(QmakeBuilder.class);
+			load();
+		}
+
+		/**
+		 * Performs on-the-fly validation of the form field 'projectFile'.
+		 *
+		 * @param value
+		 */
+		public FormValidation doCheckProjectFile(@QueryParameter final String value) throws IOException, ServletException {
+			if(value.length()==0)
+				return FormValidation.error("Please set a project file");
+			if(value.length() < 1)
+				return FormValidation.warning("Isn't the name too short?");
+
+			File file = new File(value);
+			if (file.isDirectory())
+				return FormValidation.error("Project file is a directory");
+
+			if (file.getName().endsWith(".pri"))
+				return FormValidation.error("Project includes cannot be used to build a project");
+
+			if (!file.getName().endsWith(".pro"))
+				return FormValidation.warning("Project file is not have .pro extension");
+
+			return FormValidation.ok();
+		}
+
+		/**
+		 * This human readable name is used in the configuration screen.
+		 */
+		public String getDisplayName() {
+			return "QMake Build";
+		}
+
+		@Override
+		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+			return true;
+		}
+
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject o) throws FormException {
+			// to persist global configuration information,
+			// set that to properties and call save().
+			qmakePath = o.getString("qmakePath");
+			makeCmdUnix = o.getString("makeCmdUnix");
+			makeCmdWindows = o.getString("makeCmdWindows");
+			save();
+			return super.configure(req, o);
+		}
+
+		public String getQmakePath() {
+			return this.qmakePath;
+		}
+
+		public String getMakeCmdUnix() {
+			return this.makeCmdUnix;
+		}
+
+		public String getMakeCmdWindows() {
+			return this.makeCmdWindows;
+		}
+
+	}
 }
 
--- src/main/java/hudson/plugins/qmake/QmakeBuilderImpl.java
+++ src/main/java/hudson/plugins/qmake/QmakeBuilderImpl.java
@@ -3,66 +3,48 @@ package hudson.plugins.qmake;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
-import java.util.Set;
-import java.io.PrintStream;
+import hudson.FilePath;
+public
+class QmakeBuilderImpl {
 
-public class QmakeBuilderImpl {
+	private static final String QMAKE_DEFAULT = "qmake";
 
-  private static final String QMAKE_DEFAULT = "qmake";
+	String qmakeBin;
+	boolean isWindows;
 
-  String qmakeBin;
-  boolean isWindows;
+	public QmakeBuilderImpl() {
+		super();
+	}
 
-  public QmakeBuilderImpl() {
-    super();
-  }
-	
-  String preparePath(Map<String, String> envVars, String path, boolean isWindows, PrintStream logger) throws IOException {
-    this.isWindows = isWindows;
-    path = path.trim();
+	void setQmakeBin(Map<String, String> envVars,
+			String globalQmakeBin, boolean isWindows) throws IOException, InterruptedException {
+		qmakeBin = QMAKE_DEFAULT;
 
-    Set<String> keys = envVars.keySet();
-    for (String key : keys) {
-      path = path.replaceAll("\\$" + key, envVars.get(key));
-    }
+		if (globalQmakeBin != null && globalQmakeBin.length() > 0) {
+		    File fileInfo = new File( globalQmakeBin );
+		    if (fileInfo.exists())
+            qmakeBin = fileInfo.toString();
+		}
 
-    File file = new File(path);
-    if (!file.isAbsolute()) {
-      String tmp = path;
-      path = envVars.get("WORKSPACE");
-      if (isWindows) path += "\\";
-      else path += "/";
-      path += tmp;
-    }
+		if (envVars.containsKey("QTDIR")) {
+			String checkName = envVars.get("QTDIR");
+			if (isWindows)
+				checkName += "\\bin\\qmake.exe";
+			else
+				checkName += "/bin/qmake";
 
-    return path;
-  }
+			File fileInfo = new File(checkName);
+			if (fileInfo.exists())
+				qmakeBin = fileInfo.toString();
+		}
+	}
 
-  void setQmakeBin(Map<String, String> envVars,
-                   String globalQmakeBin, boolean isWindows ) {
-    qmakeBin = QMAKE_DEFAULT;
+	String buildQMakeCall(FilePath projectFile, String extraArguments) {
+		String qmakeCall = qmakeBin + " -r \"" + projectFile + "\"";
 
-    if (globalQmakeBin != null && globalQmakeBin.length() > 0) {
-      File fileInfo = new File( globalQmakeBin );
-      if (fileInfo.exists()) qmakeBin = globalQmakeBin;
-    }
-
-    if (envVars.containsKey( "QTDIR" ) ) {
-      String checkName = envVars.get("QTDIR");
-      if (isWindows) checkName += "\\bin\\qmake.exe";
-      else           checkName += "/bin/qmake";
-
-      File fileInfo = new File( checkName );
-      if (fileInfo.exists()) qmakeBin = checkName;
-    }
-  }
-
-  String buildQMakeCall(String projectFile, String extraArguments ) {
-    String qmakeCall = qmakeBin + " -r \"" + projectFile + "\"";
-		     
-    if (!extraArguments.isEmpty()) {
-      qmakeCall += " " + extraArguments;
-    }
-    return qmakeCall;
-  }
+		if (!extraArguments.isEmpty()) {
+			qmakeCall += " " + extraArguments;
+		}
+		return qmakeCall;
+	}
 }
